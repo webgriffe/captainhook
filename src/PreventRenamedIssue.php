1fe325498c43f13ca6e1098532ee173db550f5ce
Giacomo Moscardini <gmoscardini@webgriffe.com>
2019-04-15 14:25:34 +0200

    [WIP] Adds case-sensitive file name verification

---
+++ src/PreventRenamedIssue.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Webgriffe\CaptainHook;
+
+use CaptainHook\App\Config;
+use CaptainHook\App\Console\IO;
+use CaptainHook\App\Hook\Action;
+use SebastianFeldmann\Git\Repository;
+
+class PreventRenamedIssue implements Action
+{
+
+    /**
+     * Executes the action.
+     *
+     * @param \CaptainHook\App\Config $config
+     * @param \CaptainHook\App\Console\IO $io
+     * @param \SebastianFeldmann\Git\Repository $repository
+     * @param \CaptainHook\App\Config\Action $action
+     * @return void
+     * @throws \Exception
+     */
+    public function execute(Config $config, IO $io, Repository $repository, Config\Action $action): void
+    {
+        //TODO: prevent mac user to rename a file with only case change
+        //TODO: prevent linux user to create a second file with only case change in the same directory
+    }
+
+}
